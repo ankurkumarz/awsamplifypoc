cc96215c001757d46985e47cc06a85e1e6cf5083
ankur.kumar <Ankur.Kumar@cetera.com>
2021-02-01 18:57:06 -0800

    updated react app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>React App POC - Ankur</h1>
+      </header>
+    </div>
+  );
+}
+
+function OldApp() {
   return (
     <div className="App">
       <header className="App-header">
